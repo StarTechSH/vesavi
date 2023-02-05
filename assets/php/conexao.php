ef5e570bb2a0f1fe05c71a0f59d23e412112c6d6
StarTechSH <118080715+StarTechSH@users.noreply.github.com>
2023-02-04 21:49:28 -0300

    Add files via upload

---
+++ assets/php/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = "br-asc-web880.main-hosting.eu";
+$usuario = "u916021392_adm";
+$senha = "Tiadm123@";
+$dbname = "u916021392_clientes";
+$port = 3306;
+$conn = new mysqli($servidor, $usuario, $senha, $dbname);
