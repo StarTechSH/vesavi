530aa47c8f2717137f6c8e480577dadd498d6ff5
StarTechSH <118080715+StarTechSH@users.noreply.github.com>
2023-02-06 14:58:32 -0300

    Add files via upload

---
+++ assets/js/popup.js
@@ -0,0 +1,25 @@
+import { nomes } from "./nomes.js"
+
+$(document).ready(function () {
+
+    setInterval(() => {
+        var pe = Math.floor(Math.random() * nomes.length);
+        iziToast.info({
+            id: "info",
+            title: nomes[pe],
+            message: "Está confirmando sua documentacao",
+            position: "topCenter"
+        })
+    }, 60000);
+
+    setInterval(() => {
+        var pe = Math.floor(Math.random() * nomes.length);
+        iziToast.success({
+            id: "success",
+            title: nomes[pe],
+            message: "Teve seu empréstimo aprovado!",
+            position: "topCenter"
+           })
+    }, 300000);
+    
+})
